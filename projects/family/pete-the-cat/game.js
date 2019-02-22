957adc6f91cacf875bbd8cd7d60d86083501b00d
Michael Tenney <michaeltenney@live.com>
2019-02-21 23:34:08 -0600

    Test platform mapping

--- projects/family/pete-the-cat/game.js
+++ projects/family/pete-the-cat/game.js
@@ -25,29 +25,18 @@ function addItems() {
   createItem(225, 200, 'red-button');
   createItem(125, 50, 'blue-button');
   createItem(350, 250, 'teal-button');
-  //createItem(375, 100, 'poison');
-  //createItem(370,500,'poison');
-  //createItem(100, 375, 'poison');
-  // createItem(125, 50, 'star');
 }
 
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  //platforms.create(450, 550, 'platform');
-  //platforms.create(100, 550, 'platform');
-
-    platforms.create(300, 490, 'car' );
-    platforms.create(100, 150, 'platform');
-	platforms.create(600, 420, 'platform2'); //wood right
-	platforms.create(400, 260, 'platform2'); //wood left
-    platforms.create(50, 300, 'platform'); 
-  //platforms.create(150, 250, 'platform');
-  //platforms.create(650, 300, 'platform');
-  //platforms.create(550, 200, 'platform2');
-
-  
-  
+  platforms.create(200, 100, 'platform2'); //cloud
+  // platforms.create(400, 260, 'platform2'); //cloud
+  // platforms.create(400, 260, 'platform2'); //cloud
+  // //platforms.create(300, 490, 'car' );
+  // platforms.create(100, 150, 'platform'); //grass
+  // platforms.create(50, 300, 'platform'); //grass
+  // platforms.create(50, 300, 'platform'); //grass
   platforms.setAll('body.immovable', true);
 }
 
@@ -70,14 +59,14 @@ function createBadge() {
 function itemHandler(player, item) {
   item.kill();
   if (item.key === 'blue-button' || 'red-button' || 'green-button' || 'teal-button') {
-     currentScore = currentScore + 10;
+    currentScore = currentScore + 10;
   } else if (item.key === 'poison') {
-     currentScore = currentScore - 25;
+    currentScore = currentScore - 25;
   } else if (item.key === 'star') {
-     currentScore = currentScore + 25;
+    currentScore = currentScore + 25;
   }
   if (currentScore === winningScore) {
-      createBadge();
+    createBadge();
   }
 }
 
@@ -90,31 +79,30 @@ function badgeHandler(player, badge) {
 // setup game when the web page loads
 window.onload = function () {
   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-  
+
   // before the game begins
   function preload() {
-     
-    
+
     //Load images
-	this.game.load.image("title", "background.png");
+    this.game.load.image("title", "background.png");
     game.load.image('platform', 'platform_3.png');
     game.load.image('platform2', 'platform_1.png');
-	game.load.image('car', 'platform_4.png');
-    
+    //game.load.image('car', 'platform_4.png');
+
     //Load spritesheets
     game.load.spritesheet('player', 'pete-larger.png', 87.5, 113);
     game.load.spritesheet('blue-button', 'blue-button.png', 36, 44);
-	game.load.spritesheet('red-button', 'red-button.png', 36, 44);
-	game.load.spritesheet('green-button', 'green-button.png', 36, 44);
-	game.load.spritesheet('teal-button', 'teal-button.png', 36, 44);
+    game.load.spritesheet('red-button', 'red-button.png', 36, 44);
+    game.load.spritesheet('green-button', 'green-button.png', 36, 44);
+    game.load.spritesheet('teal-button', 'teal-button.png', 36, 44);
     game.load.spritesheet('poison', 'poison.png', 32, 32);
     game.load.spritesheet('star', 'star.png', 32, 32);
-		 game.load.spritesheet('badge', 'badge.png', 42, 42);
+    game.load.spritesheet('badge', 'badge.png', 42, 42);
   }
 
   // initial game set up
   function create() {
-	this.titleScreenImage = this.add.sprite(0, 0, "title");
+    this.titleScreenImage = this.add.sprite(0, 0, "title");
     player = game.add.sprite(50, 600, 'player');
     player.animations.add('walk');
     player.anchor.setTo(0.5, 1);
@@ -156,7 +144,7 @@ window.onload = function () {
     else {
       player.animations.stop();
     }
-    
+
     if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down)) {
       player.body.velocity.y = -400;
     }
