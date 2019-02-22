c640c53f3f7528e8770f546c3867d29ad0b8021b
Michael Tenney <michaeltenney@live.com>
2019-02-22 00:30:15 -0600

    added skateboard

--- projects/family/pete-the-cat/game.js
+++ projects/family/pete-the-cat/game.js
@@ -22,8 +22,8 @@ function addItems() {
   createItem(450, 300, 'blue-button');
   createItem(415, 100, 'green-button');
   createItem(140, 320, 'green-button');
-  createItem(100, 250, 'red-button');
-  createItem(100, 250, 'red-button');
+  createItem(320, 450, 'red-button');
+  createItem(600, 75, 'red-button');
   createItem(100, 250, 'red-button');
 }
 
@@ -33,10 +33,10 @@ function addPlatforms() {
   platforms.create(100, 200, 'platform2'); //cloud
   platforms.create(350, 150, 'platform2'); //cloud
   platforms.create(565, 275, 'platform2'); //cloud
-  // platforms.create(300, 490, 'car' );
+  platforms.create(250, 490, 'skateboard'); //skateboard
   platforms.create(25, 350, 'platform'); //grass
   platforms.create(400, 350, 'platform'); //grass
-  platforms.create(275, 450, 'platform'); //grass
+  platforms.create(675, 225, 'platform'); //grass
   platforms.setAll('body.immovable', true);
 }
 
@@ -83,7 +83,7 @@ window.onload = function () {
     this.game.load.image("title", "background.png");
     game.load.image('platform', 'platform_3.png');
     game.load.image('platform2', 'platform_1.png');
-    //game.load.image('car', 'platform_4.png');
+    game.load.image('skateboard', 'platform_2.png');
 
     //Load spritesheets
     game.load.spritesheet('player', 'pete-larger.png', 87.5, 113);
@@ -91,9 +91,6 @@ window.onload = function () {
     game.load.spritesheet('red-button', 'red-button.png', 36, 44);
     game.load.spritesheet('green-button', 'green-button.png', 36, 44);
     game.load.spritesheet('teal-button', 'teal-button.png', 36, 44);
-    game.load.spritesheet('poison', 'poison.png', 32, 32);
-    game.load.spritesheet('star', 'star.png', 32, 32);
-    game.load.spritesheet('badge', 'badge.png', 42, 42);
   }
 
   // initial game set up
@@ -111,8 +108,8 @@ window.onload = function () {
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    text = game.add.text(16, 16, "SCORE: " + currentScore, { font: "bold 24px Arial", fill: "white" });
-    winningMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 48px Arial", fill: "white" });
+    text = game.add.text(16, 16, "SCORE: " + currentScore, { font: "bold 24px Schoolbell", fill: "#D84835" });
+    winningMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 60px Schoolbell", fill: "#D84835" });
     winningMessage.anchor.setTo(0.5, 1);
   }
 
